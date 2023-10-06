8506c93c2ec0c0bc3f88ed30a955b213440e79d9
Tran Duc Linh <duclinhlmht92@gmail.com>
2023-10-06 10:17:53 +0700

    Get Date

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useEffect } from "react";
+import Database from "./Database";
+import DetailScreen from "./screens/DetailScreen";
+import AddScreen from "./screens/AddScreen";
+import HomeScreen from "./screens/HomeScreen";
+import SearchScreen from "./screens/SearchScreen";
+import EditScreen from "./screens/EditScreen";
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const HomeNavigation = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Home" component={HomeScreen} />
+    {/* <Tab.Screen name="Add Hike" component={EntryScreen} /> */}
+    <Tab.Screen name="Search" component={SearchScreen} />
+  </Tab.Navigator>
+);
+
+const App = () => {
+  // TODO: Implement Bottom Tab navigation
+  useEffect(() => {
+    Database.initDatabase();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Back"
+          component={HomeNavigation}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Add Hike" component={AddScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen name="Edit" component={EditScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
